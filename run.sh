9af38d32ff99aee13ca30bc7bca345dfac844c8b
Mathias Goyheneche <mathias.goyheneche@gmail.com>
2015-01-14 15:49:18 +0100

    init

---
+++ run.sh
@@ -0,0 +1,11 @@
+PYTHON_DIR=py3.3_env
+
+# Activate Python 3
+. $PYTHON_DIR/bin/activate
+
+# Set python home after activate
+export PYTHONHOME=$PYTHON_DIR
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:lib/:$PYTHONHOME/lib
+
+$PYTHONHOME/bin/python fileModelBrowser.py
